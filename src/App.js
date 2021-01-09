8ef6547ce57ad8dd652a8356fe2e59c1d23e8439
adeola <adeola@adeolas-MBP.fios-router.home>
2021-01-09 17:34:34 -0500

    set up menu items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
 
 function App() {
 	return (
 		<div className="App">
-			<HomePage />
+			<Switch>
+				<Route exact path="/">
+					<HomePage />
+				</Route>
+				<Route path="/hats">
+					<HomePage />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -40,12 +40,13 @@ const Directory = () => {
 
 	return (
 		<div className="directory-menu">
-			{sections.map(({ title, imageUrl, id, size }) => (
+			{sections.map(({ title, imageUrl, id, size, linkUrl }) => (
 				<MenuItem
 					key={id}
 					title={title}
 					imageUrl={imageUrl}
 					size={size}
+					linkUrl={linkUrl}
 				/>
 			))}
 		</div>
