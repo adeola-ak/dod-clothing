24b8aec534252705773b577db61becb4cf8ddfc8
adeola <adeola@adeolas-MBP.fios-router.home>
2021-01-21 00:13:50 -0500

    edited shop reducer and shop page component to store shop data from firestore

--- src/pages/shoppage/ShopPage.js
+++ src/pages/shoppage/ShopPage.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CollectionsOverview from "../../components/collections-overview/CollectionsOverview";
 import { Route } from "react-router-dom";
 import Collection from "../collection/Collection";
+import {
+	firestore,
+	convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase-utils";
+import { connect } from "react-redux";
+import { updateCollections } from "../../redux/shop/shop.actions";
 
-const ShopPage = ({ match }) => {
-	console.log(match);
-	return (
-		<div className="shop-page">
-			<Route
-				exact
-				path={`${match.path}`}
-				component={CollectionsOverview}
-			/>
-			<Route
-				path={`${match.path}/:collectionId`}
-				component={Collection}
-			/>
-		</div>
-	);
-};
-export default ShopPage;
+class ShopPage extends React.Component {
+	unsubscribeFromSnapshot = null;
+
+	componentDidMount() {
+		const { updateCollections } = this.props;
+		const collectionRef = firestore.collection("collections");
+
+		collectionRef.get().then((snapshot) => {
+			const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+			updateCollections(collectionsMap);
+		});
+	}
+
+	render() {
+		const { match } = this.props;
+
+		return (
+			<div className="shop-page">
+				<Route
+					exact
+					path={`${match.path}`}
+					component={CollectionsOverview}
+				/>
+				<Route
+					path={`${match.path}/:collectionId`}
+					component={Collection}
+				/>
+			</div>
+		);
+	}
+}
+
+const mapDispatchToProps = (dispatch) => ({
+	updateCollections: (collectionsMap) =>
+		dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
